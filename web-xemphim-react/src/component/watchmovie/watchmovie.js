7a3dcb384e67ab03143de27226658c9bc72fa112
m1ke193 <mndx1t@gmail.com>
2023-06-29 12:18:14 +0700

    update-embed-responsive

--- web-xemphim-react/src/component/watchmovie/watchmovie.js
+++ web-xemphim-react/src/component/watchmovie/watchmovie.js
@@ -2,63 +2,60 @@ import React, { useState } from "react";
 import { useLocation } from 'react-router-dom';
 import './watchmovie.css';
 
-function WatchMovie(){
-    const {state}  = useLocation();
-    const [e,sete] = useState('1');
-    
-    function clickes(e,index)
-    {
+function WatchMovie() {
+    const { state } = useLocation();
+    const [e, sete] = useState('1');
+
+    function clickes(e, index) {
         sete(index)
         e.stopPropagation();
     }
 
-    function scrollToTopmovie(){
+    function scrollToTopmovie() {
         document.body.scrollTop = 100;
         document.documentElement.scrollTop = 100;
     };
 
     scrollToTopmovie()
-    return(
+    return (
         <div className="watch-movie">
             <div className="watch-movie-cover">
                 <div className="watch-movie-play">
-                    {state.index ==null ?
-                    <iframe className="custom-video-watch-movie" src={'https://www.2embed.ru/embed/tmdb/movie?id='+state.id} frameBorder="0" allowFullScreen title="Movie" aria-hidden="true"/>:
-                    <iframe className="custom-video-watch-movie" src={'https://www.2embed.ru/embed/tmdb/tv?id='+state.id+'&s='+state.index+'&e='+e} frameBorder="0" allowFullScreen title="TV" aria-hidden="true"/>
-                    } 
+                    {state.index == null ?
+                        <iframe className="custom-video-watch-movie" src={'https://embed.smashystream.com/playere.php?tmdb=' + state.id} frameBorder="0" allowFullScreen title="Movie" aria-hidden="true" /> :
+                        <iframe className="custom-video-watch-movie" src={'https://embed.smashystream.com/playere.php?tmdb=' + state.id + '&season=' + state.index + '&episode=' + e} frameBorder="0" allowFullScreen title="TV" aria-hidden="true" />
+                    }
                 </div>
                 <div className="watch-movie-title-es">
                     <div className="watch-movie-title">
                         <span>{state.name}</span>
                     </div>
-                { state.index ==null ? 
-                    <></>:
-                <>
-                    <div className="watch-movie-season-es">
-                        <span>{state.ss} - </span>
-                        <span>Episodes {e}</span>
-                    </div>
-                    <div className="watch-movie-es-list">
-                        {
-                            Array.from(Array(state.es), (key, index) => {
-                                    if(e === 1)
+                    {state.index == null ?
+                        <></> :
+                        <>
+                            <div className="watch-movie-season-es">
+                                <span>{state.ss} - </span>
+                                <span>Episodes {e}</span>
+                            </div>
+                            <div className="watch-movie-es-list">
                                 {
-                                        return(
-                                            <div index = {index} onClick={(e)=> {clickes(e,index+1)}}  className= {index+1 === 1 ? "list-item-es active" : "list-item-es"}>{index+1}</div>
-                                        )
+                                    Array.from(Array(state.es), (key, index) => {
+                                        if (e === 1) {
+                                            return (
+                                                <div index={index} onClick={(e) => { clickes(e, index + 1) }} className={index + 1 === 1 ? "list-item-es active" : "list-item-es"}>{index + 1}</div>
+                                            )
+                                        }
+                                        else {
+                                            return (
+                                                <div index={index} onClick={(e) => { clickes(e, index + 1) }} className={e === index + 1 ? "list-item-es active" : "list-item-es"}>{index + 1}</div>
+                                            )
+                                        }
+                                    }
+                                    )
                                 }
-                                else
-                                {
-                                        return(
-                                            <div index = {index} onClick={(e)=> {clickes(e,index+1)}} className= {e === index+1 ? "list-item-es active" : "list-item-es"}>{index+1}</div>
-                                        )
-                                }  
-                            }
-                            )
-                        }
-                    </div>
-                </>
-                }
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </div>
